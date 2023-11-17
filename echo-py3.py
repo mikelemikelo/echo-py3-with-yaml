3bc343ea97122edc45a94978656f6194e097318b
Miguel Rosas <mikelemikelo@gmail.com>
2023-11-17 10:15:31 -0700

    Adding random yaml and source file

---
+++ echo-py3.py
@@ -0,0 +1,16 @@
+import sys as sys
+
+
+# modelop.score
+def action(datum):
+	sys.stdout.flush()
+	print(datum)
+
+	yield datum
+
+# modelop.metrics
+def dict_metrics(datum):
+	yield {
+		"foo": 1,
+		"bar": "test result"
+	}
